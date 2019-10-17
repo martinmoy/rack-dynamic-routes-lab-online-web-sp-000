d75422efa812439bcffc29f33db5cd6c2aea80a0
Martin Moy <martinmoy1991@gmail.com>
2019-10-17 09:51:44 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,4 +3,15 @@ class Application
   def call(env)
      resp = Rack::Response.new
      req = Rack::Request.new(env)
+
+
+     if req.path.match()
+     else
+       resp.write "Route not found"
+      resp.status = 404
+       
+
+
+
+     
    end 
