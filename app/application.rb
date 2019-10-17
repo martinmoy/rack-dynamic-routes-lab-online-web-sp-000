0077610d0d9b49e3f06fed4f07548a5f65b5318f
Martin Moy <martinmoy1991@gmail.com>
2019-10-17 10:02:06 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,12 +5,19 @@ class Application
      req = Rack::Request.new(env)
 
 
-     if req.path== "/items/<ITEM NAME>"
-     else
+     if req.path.match(/items/)
+       item_name =req.path.split("/items/").last
+       item = @@items.find{|item| item.name ==item_name}
+       if @@items.include?(item)
+          resp.write item.price
+        else
+          resp.status = 400
+          resp.write "Item not found"
+        end
+      else
        resp.write "Route not found"
        resp.status = 404
-     end
-
-     resp.finish
+      end
+      resp.finish
    end
  end
